a17a0f4d949d0ec624e0ac1d97a1c4e2f4124aa2
Jeffery <42172238+dreamhooho@users.noreply.github.com>
2023-01-05 16:16:26 +0800

    Update index.cjs
    1

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxb34ab9ffa9603f3b',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '002445b8344c5eee6c82937400f5afeb',
 
   PROVINCE: '广东',
   CITY: '惠州',
